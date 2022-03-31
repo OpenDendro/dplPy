747705c0b5719dad4b34af1e1b5a21ae84b68995
Ifeoluwa Ale <ifeoluwaale@email.arizona.edu>
2022-03-30 17:27:01 -0700

    Implemented residual and difference in detrend

--- src/detrend.py
+++ src/detrend.py
@@ -7,30 +7,27 @@ from smoothingspline import spline
 # In the future, detrend will probably only take a series as input
 def detrend(series_data):
     for series_name, data in series_data.items():
+        print(series_name)
         nullremoved_data = data.dropna()
         yi = spline(nullremoved_data)
-        print(nullremoved_data)
-        print(yi)
-
-        detrended = residual(nullremoved_data, yi)
-
-        print(detrended)
-        break
+        
+        residual(nullremoved_data, yi)
+        difference(nullremoved_data, yi)
     
 def residual(series, yi):
     x = series.index.to_numpy()
     y = series.to_numpy()
     res = yi/y
 
-    plt.plot(x, y, "o", x, yi, "-")
+    plt.plot(x, res, "o", x, res, "-")
     plt.show()
-    return yi
+    return res
 
 def difference(series, yi):
     x = series.index.to_numpy()
     y = series.to_numpy()
     res = yi - y
 
-    plt.plot(x, y, "o", x, yi, "-")
+    plt.plot(x, res, "o", x, res, "-")
     plt.show()
-    return yi
+    return res
