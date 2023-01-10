833dd772d3c359afac429d9068546939a735e042
Ifeoluwa Ale <ifeoluwaale@email.arizona.edu>
2023-01-10 01:02:08 -0700

    Cleaning up and commenting related to xdate

--- src/test_xdate.py
+++ src/test_xdate.py
@@ -2,8 +2,8 @@ from xdate import xdate
 import dplpy as dpl
 
 def main():
-    data = dpl.readers("../tests/data/rwl/ca533.rwl")
-    xdate(data, slide_period=50, bin_floor=100)
+    data = dpl.readers("../tests/data/rwl/xDateRTest.rwl")
+    print(xdate(data, slide_period=50, bin_floor=10))
     print("Done.")
     
 main()
--- src/xdate.py
+++ src/xdate.py
@@ -1,3 +1,39 @@
+__copyright__ = """
+   dplPy for tree ring width time series analyses
+   Copyright (C) 2022  OpenDendro
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+__license__ = "GNU GPLv3"
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Date: 01/07/2023
+# Author: Ifeoluwa Ale
+# Title: xdate.py
+# Project: OpenDendro dplPy
+# Description: Crossdating function for dplPy datasets.
+#
+# example usage from Python Console: 
+# >>> import dplpy as dpl 
+# >>> data = dpl.readers("../tests/data/csv/file.csv")
+# >>> dpl.xdate(data)
+# >>> dpl.xdate(data, prewhiten=False, corr="Pearson")
+# >>> dpl.xdate(data, slide_period=50, bin_floor=10)
+
 from detrend import detrend
 from autoreg import ar_func_series
 from chron import chron
@@ -5,12 +41,11 @@ import pandas as pd
 import numpy as np
 import scipy
 
+# Main crossdating function, returns a dictionary that maps each series to its correlation against
+# a chronology of the other series.
 def xdate(data, prewhiten=True, corr="Spearman", ar_lag=5, slide_period=50, bin_floor=100):
     # Identify first and last valid indexes, for separating into bins.
-
-    print("Bins: ")
     bins = get_bins(data.first_valid_index(), data.last_valid_index(), bin_floor, slide_period)
-    print(bins)
 
     rwi_data = detrend(data, fit="horizontal", plot=False)
 
@@ -18,8 +53,8 @@ def xdate(data, prewhiten=True, corr="Spearman", ar_lag=5, slide_period=50, bin_
     if isinstance(rwi_data, ValueError) or isinstance(rwi_data, TypeError):
         raise rwi_data
 
+    # drop nans, prewhiten series if necessary
     ready_series = {}
-
     for series in rwi_data:
         nullremoved_data = rwi_data[series].dropna()
         if prewhiten is True:
@@ -35,23 +70,27 @@ def xdate(data, prewhiten=True, corr="Spearman", ar_lag=5, slide_period=50, bin_
 
     for series in ready_series:
         removed = ready_series_copy.pop(series)
+
+        # create chronology with current series removed
         new_chron = chron(ready_series_copy, plot=False)["Mean RWI"]
 
-        # edit removed and new to make sure they are the same size
+        # correlate current series against chronology of remaining series
         inp = pd.concat([removed, new_chron], axis=1, join='inner')
-
         series_corr[series] = correlate(inp, corr)
         
+        # evaluation of current series vs chronology of others by segments of years (the bins created earlier)
         for range in bins:
             start = int(range.split("-")[0])
             end = int(range.split("-")[1])
             if start >= removed.first_valid_index() and end <= removed.last_valid_index():
                 segment = removed.loc[start:end]
-                compare_segment(segment, new_chron, slide_period, corr)
+                compare_segment(segment, new_chron, slide_period, corr, slide=False)
 
         ready_series_copy[series] = removed
     return series_corr
 
+# Generates the bins given the first and last years of recorded data, the bin floor and
+# the desired number of years in a period (bin).
 def get_bins(first_year, last_year, bin_floor, slide_period):
     overlap = int(slide_period/2)
     if bin_floor == 0 or first_year % bin_floor == 0:
@@ -67,43 +106,48 @@ def get_bins(first_year, last_year, bin_floor, slide_period):
         i += overlap
     return bins
 
+# Returns the correlation value of the given data. Can find Spearman or Pearson's
+# correlations
 def correlate(data, type):
     if type == "Spearman":
         return scipy.stats.spearmanr(data, axis=0).correlation
     elif type == "Pearson":
         return np.corrcoef(data, rowvar=False)[0, 1]
     
-
-def compare_segment(segment, new_chron, slide_period, correlation_type, left_most=-10, right_most=10):
+# Compares segments 
+def compare_segment(segment, new_chron, slide_period, correlation_type, slide=True, left_most=-10, right_most=10):
     if segment.size < slide_period:
         return
     series_name = segment.name
     data = pd.concat([segment, new_chron], axis=1, join='inner')
     original = correlate(data, correlation_type)
 
+    # dplR flags correlation values < 0.25
+    # Update warning message
     if original < 0.2:
-        print("Dating issue with", segment.name, "in the range of", segment.first_valid_index(), "to", segment.last_valid_index())
+        print(segment.name, "has low (< 0.2) correlation with rest of series' chronology, from", segment.first_valid_index(), "to", segment.last_valid_index())
 
+    if not slide:
+        return 0, original
+    
     best_lag = 0
     best_coeff = original
 
     for shift in range(left_most, right_most+1):
-        shifted = data[series_name]
+        shifted = data[series_name].copy(deep=False)
         shifted.index += shift
         overlapping_df = pd.concat([shifted, new_chron], axis=1, join='inner').dropna()
         if overlapping_df.size == slide_period * 2:
-            new_coeff = np.corrcoef(overlapping_df, rowvar=False)[0, 1]
+            new_coeff = correlate(overlapping_df, correlation_type)
             if new_coeff > best_coeff:
+                # for debugging print(segment.name, segment.first_valid_index(), shift)
                 best_lag = shift
                 best_coeff = new_coeff
-    warning = True
-    """
+    
     if (best_lag != 0 and abs(best_coeff - original) >= 0.1):
         print("\nUnexpected results for", segment.name, "in the range of", segment.first_valid_index(), "to", segment.last_valid_index())
         print("Original correlation was", original)
         print("Best correlation was at lag", best_lag, "and correlation was", best_coeff, "\n")
-        warning = True
-    else:
-        warning = False
-    """
-    return (best_lag, best_coeff, warning)
+    
+    return best_lag, best_coeff
+
