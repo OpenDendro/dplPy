fb84b46c1691fd5a44736d8f0d4852971223e7db
Kevin Anchukaitis <kevin.anchukaitis@gmail.com>
2023-12-01 14:46:52 -0700

    Update __init__.py

--- dplpy/__init__.py
+++ dplpy/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "Tyson Lee Swetnam"
 __email__ = "tswetnam@arizona.edu"
-__version__ = "0.1.testing"
+__version__ = "0.1.1"
 
 __copyright__ = """
    dplPy for tree ring width time series analyses
