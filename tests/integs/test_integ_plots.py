86e467a66d47e6dd8220ca32eea7b6fde2caaabf
Ifeoluwa Ale <84350850+ifeoluwaale@users.noreply.github.com>
2023-12-24 23:30:17 -0500

    Fixed issue that caused integs to fail in previous commit

--- tests/integs/test_integ_plots.py
+++ tests/integs/test_integ_plots.py
@@ -6,23 +6,23 @@
 
 # #TODO: assert content of plots somehow
 # def test_seg_plots():
-#     ca533 = dpl.readers("../integs/data/csv/ca533.csv")
-#     ca667 = dpl.readers("../integs/data/rwl/ca667.rwl", header=True)
+#     ca533 = dpl.readers("./integs/data/csv/ca533.csv")
+#     ca667 = dpl.readers("./integs/data/rwl/ca667.rwl", header=True)
 
 #     dpl.plot(ca533, "seg")
 #     dpl.plot(ca667, "seg")
 
 
 # def test_spag_plots():
-#     ca533 = dpl.readers("../integs/data/csv/ca533.csv")
-#     ca667 = dpl.readers("../integs/data/rwl/ca667.rwl", header=True)
+#     ca533 = dpl.readers("./integs/data/csv/ca533.csv")
+#     ca667 = dpl.readers("./integs/data/rwl/ca667.rwl", header=True)
 
 #     dpl.plot(ca533, "spag")
 #     dpl.plot(ca667, "spag")
 
 # def test_line_plots():
-#     ca533 = dpl.readers("../integs/data/csv/ca533.csv")
-#     ca667 = dpl.readers("../integs/data/rwl/ca667.rwl", header=True)
+#     ca533 = dpl.readers("./integs/data/csv/ca533.csv")
+#     ca667 = dpl.readers("./integs/data/rwl/ca667.rwl", header=True)
 
 #     dpl.plot(ca533, "line")
 #     dpl.plot(ca667, "line")
--- tests/integs/test_integ_readers_and_writers.py
+++ tests/integs/test_integ_readers_and_writers.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 
 def test_read_and_write_csv(tmp_path):
-    ca533 = dpl.readers("../tests/data/csv/ca533.csv")
+    ca533 = dpl.readers("./tests/data/csv/ca533.csv")
 
     write_path = os.path.join(tmp_path,"test_write")
     
@@ -16,7 +16,7 @@ def test_read_and_write_csv(tmp_path):
 
 
 def test_read_and_write_rwl_no_headers(tmp_path):
-    viet001 = dpl.readers("../tests/data/rwl/viet001.rwl")
+    viet001 = dpl.readers("./tests/data/rwl/viet001.rwl")
 
     write_path = os.path.join(tmp_path, "test_write")
 
@@ -28,7 +28,7 @@ def test_read_and_write_rwl_no_headers(tmp_path):
 
 
 def test_read_and_write_rwl_with_headers(tmp_path):
-    th001 = dpl.readers("../tests/data/rwl/th001.rwl", header=True)
+    th001 = dpl.readers("./tests/data/rwl/th001.rwl", header=True)
 
     write_path = os.path.join(tmp_path, "test_write")
 
@@ -40,7 +40,7 @@ def test_read_and_write_rwl_with_headers(tmp_path):
 
 
 def test_read_and_write_long_rwl(tmp_path):
-    ca667 = dpl.readers("../tests/data/rwl/ca667.rwl", header=True)
+    ca667 = dpl.readers("./tests/data/rwl/ca667.rwl", header=True)
 
     write_path = os.path.join(tmp_path, "test_write")
 
@@ -51,7 +51,7 @@ def test_read_and_write_long_rwl(tmp_path):
     pd.testing.assert_frame_equal(ca667, ca667_alt)
 
 def test_read_and_write_weird_rwl(tmp_path):
-    wwr = dpl.readers("../tests/data/rwl/wwr.rwl")
+    wwr = dpl.readers("./tests/data/rwl/wwr.rwl")
 
     write_path = os.path.join(tmp_path, "test_write")
 
