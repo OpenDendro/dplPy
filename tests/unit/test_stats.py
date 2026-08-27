277337585ce0489d3aaa4c33df3b700764c116cc
Kevin Anchukaitis <kevin.anchukaitis@gmail.com>
2026-08-27 10:50:45 -0700

    Argh. readers.py ambiguity bit me again in tests.

--- tests/unit/test_stats.py
+++ tests/unit/test_stats.py
@@ -23,8 +23,9 @@ def mock_readers_output(file_name):
 
 import importlib
 _m_stats = importlib.import_module("dplpy.stats")
+_m_readers = importlib.import_module("dplpy.readers")
 
-@patch('dplpy.readers.readers')
+@patch.object(_m_readers, 'readers')
 @patch.object(AutoReg, 'fit', new=mock_auto_reg_fit)
 def test_stats_with_inp_string(mock_readers: Mock):
     mock_readers.side_effect = mock_readers_output
@@ -47,7 +48,7 @@ def test_stats_with_inp_string(mock_readers: Mock):
     pd.testing.assert_frame_equal(results, expected_df)
 
 
-@patch('dplpy.readers.readers')
+@patch.object(_m_readers, 'readers')
 @patch.object(AutoReg, 'fit', new=mock_auto_reg_fit)
 def test_stats_with_inp_df(mock_readers: Mock):
     mock_readers.side_effect = mock_readers_output
--- tests/unit/test_summary.py
+++ tests/unit/test_summary.py
@@ -16,8 +16,9 @@ def mock_dataframe_summary(self):
 
 import importlib
 _m_summary = importlib.import_module("dplpy.summary")
+_m_readers = importlib.import_module("dplpy.readers")
 
-@patch('dplpy.readers.readers')
+@patch.object(_m_readers, 'readers')
 @patch.object(pd.DataFrame, 'describe', new=mock_dataframe_summary)
 def test_summary_given_filename(mock_readers: Mock):
     mock_readers.side_effect = mock_readers_output
@@ -29,7 +30,7 @@ def test_summary_given_filename(mock_readers: Mock):
     mock_readers.assert_called_once_with("valid_file")
     pd.testing.assert_frame_equal(results, expected_df)
 
-@patch('dplpy.readers.readers')
+@patch.object(_m_readers, 'readers')
 @patch.object(pd.DataFrame, 'describe', new=mock_dataframe_summary)
 def test_summary_given_dataframe(mock_readers: Mock):
     mock_readers.side_effect = mock_readers_output
